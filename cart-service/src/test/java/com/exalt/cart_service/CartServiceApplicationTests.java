b0265a1dab0e5a779bac48ee3886f67f054c9a20
t-Mohammad Rimawi <t-mohammad.rimawi@exalt.ps>
2026-08-17 09:46:29 +0300

    Initial commit

---
+++ cart-service/src/test/java/com/exalt/cart_service/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exalt.cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
